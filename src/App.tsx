5fd0fd73b3c00a61ae65a6c64f23217bd84e0994
smic29 <sibulo1991@gmail.com>
2024-07-27 21:38:13 +0800

    Added useref for AbortController to make sure requests don't go over fixed amount

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
-import { useState, useEffect } from 'react';
+import { useState, useEffect, useRef } from 'react';
 import Table from './components/table';
 import Button from './components/button';
 
@@ -8,21 +8,29 @@ function App() {
   const [data, setData] = useState<any[]>(JSON.parse(localStorage.getItem('tasks') || '[]'))
   const [isLoading, setIsLoading] = useState<boolean>(data.length < 15)
   const [error, setError] = useState<string | null>(null);
+  const abortControllerRef = useRef<AbortController | null>(null)
   
   const fetchData = async () => {
+    if (data.length >= 15) {
+      return
+    }
+
+    abortControllerRef.current = new AbortController()
+    const signal = abortControllerRef.current.signal
+
     try {
-      const response = await fetch('/random')
+      const response = await fetch('/random', {signal})
       if (!response.ok) {
-        throw new Error('Error with response')
+        throw new Error(`${response.statusText}`)
       }
       const result = await response.json()
       
       setData(prevData => {
         const updatedData = [...prevData, result]
-
         if (updatedData.length >= 15) {
           localStorage.setItem('tasks', JSON.stringify(updatedData.slice(0, 15)))
           setIsLoading(false)
+          abortControllerRef.current?.abort()
         }
         return updatedData
       })
@@ -32,13 +40,20 @@ function App() {
       }
       
     } catch (error:any) {
-      setError(error.message)
+      if (error.name !== 'AbortError') {
+        setError(error.message)
+        setIsLoading(false)
+      }
     } 
   };
 
   useEffect(() => {
     if (isLoading) {
       fetchData()
+    }
+
+    return () => {
+      abortControllerRef.current?.abort()
     }
   }, [isLoading])
 
@@ -49,7 +64,7 @@ function App() {
 
   return (
     <div className="App container mx-auto px-4 py-4">
-      { isLoading ? "Loading Data..." : 
+      { isLoading ? `Loading Data...${data.length}/15` : error ? `Something went wrong: ${error}` :
       <>
         <Table taskData={data} />
         <div className="flex flex-row justify-center">
